6db39c9f1703e789051edbdca89718c96c0e4edf
Greg Prawdzik <gprawd@hotmail.com>
2019-11-18 14:56:49 -0600

    added html output

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ function init(){
   button.addEventListener('click', function(){
     var fc = document.getElementById('entryinput')
     alert('Gregory Prawdzik: ' + fc.value);
+    var output = document.getElementById('textoutput');
+    output.innerHTML = fc.value;
     });
 
 
